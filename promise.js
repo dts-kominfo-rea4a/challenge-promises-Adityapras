60bf500c85440eeeb5c07be1b9a753b4db5cff49
Aditya Prasetyo <prasetyoadity56@gmail.com>
2022-09-16 19:48:01 +0700

    challenge promises

--- promise.js
+++ promise.js
@@ -1,7 +1,16 @@
-const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
+const {
+  promiseTheaterIXX,
+  promiseTheaterVGC
+} = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  const ixx = await promiseTheaterIXX(emosi);
+  const vgc = await promiseTheaterVGC(emosi);
+  const mergeData = ixx.concat(vgc);
+  const result = mergeData.filter(data => data.hasil === emosi);
+  return result.length;
+};
 
 module.exports = {
   promiseOutput,
